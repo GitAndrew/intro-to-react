1ec0e017f6bc273d2f21384fb52099d0b131c98f
Andrew <andrewchan077@gmail.com>
2021-05-09 18:45:28 +1000

    1 - First React Component

---
+++ index.js
@@ -0,0 +1,7 @@
+class App extends React.Component {
+  render() {
+    return React.createElement("div", null, "Hello World!");
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
