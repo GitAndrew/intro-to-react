d5a782105a4d3db22d16e48cbc0e8ada09dd1888
Andrew <andrewchan077@gmail.com>
2021-05-22 19:23:22 +1000

    7 - Adding data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,37 +6,59 @@ class Home extends React.Component {
     super(props);
     this.state = {
       movies: [],
+      loading: false,
     };
   }
 
   componentDidMount() {
-    this.setState({
-      movies: [
-        {
-          id: "1",
-          title: "First Card",
-          description: "This is a card",
-        },
-        {
-          id: "2",
-          title: "Second Card",
-          description: "This is another card",
-        },
-      ],
-    });
+    // this.setState({
+    //   loading: true,
+    // });
+    // fetch(`http://localhost:9000/movies`)
+    //   .then((res) => res.json())
+    //   .then((movies) => {
+    //     this.setState({
+    //       movies,
+    //     });
+    //   })
+    //   .catch((e) => {})
+    //   .finally(() => {
+    //     this.setState({
+    //       loading: false,
+    //     });
+    //   });
+    (async () => {
+      this.setState({
+        loading: true,
+      });
+      try {
+        const response = await fetch(`http://localhost:9000/movies`);
+        const movies = await response.json();
+        this.setState({
+          movies,
+        });
+      } catch (e) {}
+      this.setState({
+        loading: false,
+      });
+    })();
   }
 
   render() {
     return (
       <div>
         <div>Home Page</div>
-        {this.state.movies.map((movie) => (
-          <Card
-            key={movie.id}
-            title={movie.title}
-            description={movie.description}
-          />
-        ))}
+        {this.state.loading ? (
+          <div>Loading</div>
+        ) : (
+          this.state.movies.map((movie) => (
+            <Card
+              key={movie.id}
+              title={movie.title}
+              description={movie.description}
+            />
+          ))
+        )}
       </div>
     );
   }
