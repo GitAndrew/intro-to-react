b1c4c27a88ddd879b04523c54fc4571721c7b262
Andrew <andrewchan077@gmail.com>
2021-05-22 19:00:26 +1000

    6 - Adding State

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,12 +2,41 @@ import React from "react";
 import { Card } from "components";
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      movies: [
+        {
+          id: "1",
+          title: "First Card",
+          description: "This is a card",
+        },
+        {
+          id: "2",
+          title: "Second Card",
+          description: "This is another card",
+        },
+      ],
+    });
+  }
+
   render() {
     return (
       <div>
         <div>Home Page</div>
-        <Card title="First Card" description="This is a card" />
-        <Card title="Second Card" description="This is another card" />
+        {this.state.movies.map((movie) => (
+          <Card
+            key={movie.id}
+            title={movie.title}
+            description={movie.description}
+          />
+        ))}
       </div>
     );
   }
