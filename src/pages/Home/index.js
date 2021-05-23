4f7f149790db2aaf090d55d084c0273b69960b12
Andrew <andrewchan077@gmail.com>
2021-05-23 15:17:04 +1000

    12 - Refactoring Home component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Card, Search } from "components";
 import logo from "images/logo.png";
 import {
@@ -10,97 +10,64 @@ import {
   Content,
   SearchContainer,
 } from "./styles";
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: [],
-      loading: false,
-      searchFilter: "",
-    };
-    // this.searchFilterOnChange = this.searchFilterOnChange.bind(this);
-  }
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchFilter, setSearchFilter] = useState("");
 
-  componentDidMount() {
-    // this.setState({
-    //   loading: true,
-    // });
-    // fetch(`http://localhost:9000/movies`)
-    //   .then((res) => res.json())
-    //   .then((movies) => {
-    //     this.setState({
-    //       movies,
-    //     });
-    //   })
-    //   .catch((e) => {})
-    //   .finally(() => {
-    //     this.setState({
-    //       loading: false,
-    //     });
-    //   });
+  useEffect(() => {
     (async () => {
-      this.setState({
-        loading: true,
-      });
+      setLoading(true);
       try {
         const response = await fetch(`http://localhost:9000/movies`);
         const movies = await response.json();
-        this.setState({
-          movies,
-        });
+        setMovies(movies);
       } catch (e) {}
-      this.setState({
-        loading: false,
-      });
+      setLoading(false);
     })();
-  }
+  }, []);
 
-  // Use an arrow function instead of a regular function to avoid having to bind the this keyword
-  searchFilterOnChange = (event) => {
-    this.setState({
-      searchFilter: event.target.value,
-    });
+  const searchFilterOnChange = (event) => {
+    setSearchFilter(event.target.value);
   };
 
-  render() {
-    const filteredMovies = this.state.movies.filter((movie) =>
-      movie.title.toLowerCase().includes(this.state.searchFilter.toLowerCase())
-    );
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchFilter.toLowerCase())
+  );
 
-    return (
-      <MainContainer>
-        <Header>
-          <Title>Studio Ghibli Movie Watchlist</Title>
-          <LogoContainer>
-            <Logo src={logo} />
-          </LogoContainer>
-        </Header>
-        <SearchContainer>
-          <Search
-            value={this.state.searchFilter}
-            onChange={this.searchFilterOnChange}
-            placeholder={"Search Movies"}
-          />
-        </SearchContainer>
-        <Content>
-          {this.state.loading ? (
-            <div>Loading</div>
-          ) : (
-            filteredMovies.map((movie) => (
-              <Card
-                key={movie.id}
-                originalTitle={movie.original_title}
-                title={movie.title}
-                description={movie.description}
-                releaseDate={movie.release_date}
-                backgroundImage={movie.cover}
-              />
-            ))
-          )}
-        </Content>
-      </MainContainer>
-    );
-  }
-}
+  return (
+    <MainContainer>
+      <Header>
+        <Title>Studio Ghibli Movie Watchlist</Title>
+        <LogoContainer>
+          <Logo src={logo} />
+        </LogoContainer>
+      </Header>
+      <SearchContainer>
+        <Search
+          value={searchFilter}
+          onChange={searchFilterOnChange}
+          placeholder={"Search Movies"}
+        />
+      </SearchContainer>
+      <Content>
+        {loading ? (
+          <div>Loading</div>
+        ) : (
+          filteredMovies.map((movie) => (
+            <Card
+              key={movie.id}
+              originalTitle={movie.original_title}
+              title={movie.title}
+              description={movie.description}
+              releaseDate={movie.release_date}
+              backgroundImage={movie.cover}
+            />
+          ))
+        )}
+      </Content>
+    </MainContainer>
+  );
+};
 
 export default Home;
