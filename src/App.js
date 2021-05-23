b780bbb2e7edddcec3e86aaa96f68970605f5c8b
Andrew <andrewchan077@gmail.com>
2021-05-23 17:36:26 +1000

    16 - React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Home, Movie } from "pages";
 import GlobalStyle from "./globalStyles";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      {/* <Home /> */}
-      <Movie />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies/:id" component={Movie} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </>
   );
 };
